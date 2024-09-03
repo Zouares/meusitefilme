8f1509ac14830a5ffec8466d669350ff89682bf1
Zouares <gabrielssoaress02@gmail.com>
2024-09-03 17:23:19 -0300

    INICIANDO PROJETO NETFLIX

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=''>
+        <h1 className='text-red-600 text-4x1 font-bold cursor-pointer'>NETFLIX</h1>
+        <div>
+            <button>Sign In</button>
+            <button>Sign Up</button>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
